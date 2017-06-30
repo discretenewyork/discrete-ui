50473b60a311481f1323f5337adda805b32a9394
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-06-30 12:58:41 -0400

    Refactor Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,1 @@
-import React from 'react'
-import styles from './styles.css'
-
-const Input = ({className, ...props}) => (
-  <input
-    className={[styles.input, className].join(' ')}
-    {...props}
-  />
-)
-
-export default Input
+export default from './Input'
