06e7758896428fc41d445a4ff746d5eaed2e6937
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-06-30 15:17:08 -0400

    Add color swatches

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Container, Input, Search, Button } from 'components'
 import Section from './Section'
+import Swatch from './Swatch'
 import '../theme/base.css';
 
 const MOUNT_NODE = document.querySelector('#root')
@@ -9,6 +10,16 @@ const MOUNT_NODE = document.querySelector('#root')
 const DemoView = () => (
   <div>
     <Container style={{marginTop: 64}}>
+
+          <Section
+            title='Colors'
+            description='A pallette of re-usable colors'>
+            <Swatch color='blue' />
+            <Swatch color='white' />
+            <Swatch color='darkWhite' />
+            <Swatch color='black' />
+            <Swatch color='textColor' />
+          </Section>
 
           <Section
             title='Input'
