1224db329b3651b8994a32327efd619bc3ca8b48
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-09-22 21:08:22 -0400

    Add TextArea

---
+++ src/TextArea.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const TextArea = styled.textarea`
+  display: block;
+  width: 100%;
+  min-height: 6em;
+  padding: 0.25em;
+  margin: 1em 0.25em;
+  font-size: 0.875em;
+  background: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.border};
+  font-family: ${({ theme }) => theme.font.mono};
+`
+
+TextArea.propTypes = {
+  theme: PropTypes.shape({
+    color: PropTypes.shape({
+      white: PropTypes.string,
+      border: PropTypes.string
+    }),
+    font: PropTypes.shape({
+      mono: PropTypes.string
+    })
+  })
+}
+
+TextArea.defaultProps = {
+  theme: {
+    color: {
+      white: 'white',
+      border: 'black'
+    },
+    font: {
+      mono: 'monospace'
+    }
+  }
+}
+
+export default TextArea
